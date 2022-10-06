fa151089d285aeb08f23a6d1f3c0d488f9092ad8
Critical Session <criticalsession@protonmail.ch>
2022-10-06 10:29:40 +0200

    added menu

---
+++ src/utils/utils.js
@@ -0,0 +1,20 @@
+const listenForOutsideClicks = (
+  listening,
+  setListening,
+  menuRef,
+  setIsOpen
+) => {
+  return () => {
+    if (listening) return;
+    if (!menuRef.current) return;
+    setListening(true);
+    [`click`, `touchstart`].forEach((type) => {
+      document.addEventListener(`click`, (evt) => {
+        if (menuRef.current.contains(evt.target)) return;
+        setMenuOpen(false);
+      });
+    });
+  };
+};
+
+export { listenForOutsideClicks };
